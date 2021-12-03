36edf0c4ca696733f2f7f8bd9b8848596520c284
Emily Ma <em.kayama@gmail.com>
2021-12-03 13:23:04 -0500

    finish part 2

--- 2021/day3/solution.go
+++ 2021/day3/solution.go
@@ -28,7 +28,7 @@ func getColBits(arr [][]string, idx int) []string {
 	return result
 }
 
-func getCommonBit(arr []string, isGamma bool) int {
+func getCommonBit(arr []string, isMost bool) int {
 	var result int
 	count := []int{0, 0}
 	for _, bit := range arr {
@@ -37,13 +37,13 @@ func getCommonBit(arr []string, isGamma bool) int {
 	}
 
 	if count[0] == count[1] {
-		if isGamma {
+		if isMost {
 			result = 1
 		} else {
 			result = 0
 		}
 	} else {
-		if isGamma {
+		if isMost {
 			if count[0] > count[1] {
 				result = 0
 			} else {
@@ -60,20 +60,50 @@ func getCommonBit(arr []string, isGamma bool) int {
 	return result
 }
 
-func findRating(input [][]string, isGamma bool) string {
+func findRating1(input [][]string, isMost bool) string {
 	result := ""
 	for idx := range input[0] {
 		currColArr := getColBits(input, idx)
-		result += strconv.Itoa(getCommonBit(currColArr, isGamma))
+		result += strconv.Itoa(getCommonBit(currColArr, isMost))
 	}
 
 	return result
 }
 
+func filterNums(arr [][]string, idx, bit int) [][]string {
+	var result [][]string
+	for _, num := range arr {
+		if num[idx] == strconv.Itoa(bit) {
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func findRating2(input [][]string, isMost bool) string {
+	arr := input[:]
+	idx := 0
+
+	for len(arr) > 1 {
+		colBits := getColBits(arr, idx)
+		mostCommonBit := getCommonBit(colBits, isMost)
+		arr = filterNums(arr, idx, mostCommonBit)
+
+		idx += 1
+	}
+
+	return strings.Join(arr[0], "")
+}
+
 func main() {
 	data := importFile("./input.txt")
-	gammaRate, _ := strconv.ParseInt(findRating(data, true), 2, 32)
-	epsilonRate, _ := strconv.ParseInt(findRating(data, false), 2, 32)
+	gammaRate, _ := strconv.ParseInt(findRating1(data, true), 2, 32)
+	epsilonRate, _ := strconv.ParseInt(findRating1(data, false), 2, 32)
 
 	fmt.Println(gammaRate * epsilonRate) // 4160394
+
+	oxygenRating, _ := strconv.ParseInt(findRating2(data, true), 2, 32)
+	co2Rating, _ := strconv.ParseInt(findRating2(data, false), 2, 32)
+
+	fmt.Println(oxygenRating * co2Rating) // 4125600
 }
